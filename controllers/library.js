1e17708e4704762bf8df543473a53d4c0efbc607
Chloé Touzani <chloe.touzani11@gmail.com>
2024-02-14 23:18:12 +0100

    Ajout des systèmes de notations

--- controllers/library.js
+++ controllers/library.js
@@ -1,6 +1,7 @@
 const Book = require('../models/book');
+const fs = require('fs')
 
-exports.createBook = (req, res, next) => {
+exports.createBook = (req, res) => {
   const bookObject =JSON.parse(req.body.book);
   delete bookObject._id;
   delete bookObject._userId;
@@ -12,28 +13,90 @@ exports.createBook = (req, res, next) => {
   book.save()
   .then(res.status(201).json({ message: 'Objet enregisté !'}))
   .catch(error => res.status(400).json({ error }));
-}
+};
 
-exports.modifyBook = (req, res, next) => {
-  Book.updateOne({ _id: req.params.id }, { ...req.body, _id: req.params.id })
-    .then(() => res.status(200).json({ message: 'Objet modifié !'}))
-    .catch(error => res.status(400).json({ error }));
-}
+exports.rateBook = (req, res) => {
+  Book.findOne({ _id: req.params.id })
+  .then((book) => {
+    const isAlreadyRated = book.ratings.find(
+      (book) => book.userId === req.auth.userId
+    );
 
-exports.deleteBook = (req, res, next) => {
-  Book.deleteOne({ _id: req.params.id })
-    .then(() => res.status(200).json({ message: 'Objet supprimé !'}))
-    .catch(error => res.status(400).json({ error }));
-}
+    if (!isAlreadyRated) {
+      book.ratings.push({
+        userId: req.auth.userId,
+        grade: req.body.rating,
+      });
 
-exports.getOneBook = (req, res, next) => {
+      const averageRating = (book.ratings.reduce(
+        (accumulator, currentValue) => accumulator + currentValue.grade,
+            0 )/ book.ratings.length).toFixed(2);
+      book.averageRating = averageRating;
+
+      return book.save();
+    } else {
+      res.status(401).json({ message: "Book already rated" });
+    }
+  })
+  .then((book) => res.status(201).json(book))
+  .catch((error) => res.status(500).json({ error }));
+  };
+
+exports.modifyBook = (req, res) => {
+  const bookObject = req.file ? {
+    ...JSON.parse(req.body.book),
+    imageUrl: `${req.protocol}://${req.get('host')}/images/${req.file.filename}`
+} : { ...req.body };
+
+  delete bookObject._userId;
+    Book.findOne({_id: req.params.id})
+        .then((book) => {
+            if (book.userId != req.auth.userId) {
+                res.status(401).json({ message : 'Not authorized'});
+            } else {
+                Book.updateOne({ _id: req.params.id}, { ...bookObject, _id: req.params.id})
+                .then(() => res.status(200).json({message : 'Objet modifié!'}))
+                .catch(error => res.status(401).json({ error }));
+            }
+        })
+        .catch((error) => {
+            res.status(400).json({ error });
+        });
+};
+
+exports.deleteBook = (req, res) => {
+  Book.findOne({ _id: req.params.id})
+  .then(book => {
+      if (book.userId != req.auth.userId) {
+          res.status(401).json({message: 'Not authorized'});
+      } else {
+          const filename = book.imageUrl.split('/images/')[1];
+          fs.unlink(`images/${filename}`, () => {
+              Book.deleteOne({_id: req.params.id})
+                  .then(() => { res.status(200).json({message: 'Objet supprimé !'})})
+                  .catch(error => res.status(401).json({ error }));
+          });
+      }
+  })
+  .catch( error => {
+      res.status(500).json({ error });
+  });
+};
+
+exports.getOneBook = (req, res) => {
   Book.findOne({ _id: req.params.id })
     .then(book => res.status(200).json(book))
     .catch(error => res.status(404).json({ error }));
-}
+};
 
 exports.getAllBooks = (req, res) => {
   Book.find()
     .then(book => res.status(200).json(book))
+    .catch(error => res.status(400).json({ error }));
+};
+
+exports.getBestBooks = (req, res) => {
+  Book.find().sort({ averageRating: -1 }).limit(3)
+    .then(books => res.status(200).json(books))
     .catch(error => res.status(400).json({ error }));
 }
--- routes/library.js
+++ routes/library.js
@@ -4,10 +4,14 @@ const libraryCtrl = require('../controllers/library');
 const router = express.Router();
 const multer = require('../middleware/multer-config');
 
-router.post('/', auth, multer, libraryCtrl.createBook);  
+router.post('/', auth, multer, libraryCtrl.createBook);
+router.post('/:id/rating', auth, libraryCtrl.rateBook);
 router.get('/', libraryCtrl.getAllBooks);
+router.get('/bestrating', libraryCtrl.getBestBooks);
 router.get('/:id', libraryCtrl.getOneBook);
-router.put('/:id', auth, libraryCtrl.modifyBook);
+
+router.put('/:id', auth, multer, libraryCtrl.modifyBook);
 router.delete('/:id', auth, libraryCtrl.deleteBook);
+
 
 module.exports = router;
